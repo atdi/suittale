fb04e28d7fbb59119aff74ff557bb61d9a3fb2cc
atdi <aurel.avramescu@gmail.com>
2014-12-18 00:55:23 +0100

    Remove the local server try

--- wsgi.py
+++ wsgi.py
@@ -8,8 +8,8 @@ init_app()
 #
 # Below for testing only
 #
-if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
+#if __name__ == '__main__':
+#    from wsgiref.simple_server import make_server
+#    httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
-    httpd.handle_request()
+#    httpd.handle_request()
