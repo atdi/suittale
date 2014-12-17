e66611af0acd9ee1ff45f9e186571ca7f23970f7
atdi <aurel.avramescu@gmail.com>
2014-12-18 00:52:59 +0100

    Try to make it work with mode wsgi

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Suit Tale',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
+      description='A tails about suites',
+      author='Aurel Avramescu',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['sqlalchemy',
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-from suittale import init_app, app
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
+import os
+from suittale import init_app
+from suittale import app as application
 
 init_app()
 
@@ -10,11 +9,7 @@ init_app()
 # Below for testing only
 #
 if __name__ == '__main__':
-    http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(8051)
-    IOLoop.instance().start()
-
-    #from wsgiref.simple_server import make_server
-    #httpd = make_server('localhost', 8051, application)
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
-    #httpd.handle_request()
+    httpd.handle_request()
