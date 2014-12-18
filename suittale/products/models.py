aceafe6f81f94290121e8fd0fae65eda53f86c4f
atdi <aurel.avramescu@gmail.com>
2014-12-18 22:42:19 +0100

    Add new models and change css

--- suittale/products/models.py
+++ suittale/products/models.py
@@ -17,8 +17,16 @@ class Product(BaseModel):
     name = db.Column(db.String(100), nullable=False)
     code = db.Column(db.String(100), unique=True, nullable=False)
     description = db.Column(db.String(255), nullable=True)
-    category_id = db.Column(db.String(255), db.ForeignKey('categories.id'))
+    image = db.Column(db.String(255), nullable=False)
+    thumbnail = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    currency = db.Column(db.String(3), default='RON', nullable=False)
+    category_id = db.Column(db.String(255), db.ForeignKey('categories.id'), nullable=False)
+    attributes = db.relationship("ProductAttribute", backref="product")
     __tablename__ = 'products'
 
 
-#class ProductAttribute(BaseModel):
+class ProductAttribute(BaseModel):
+    id = db.Column(db.String(255), primary_key=True, default=generate_uuid)
+    product_id = db.Column(db.String(255), db.ForeignKey('products.id'), nullable=False)
+    __tablename__ = 'product_attributes'
