3c29e58f5a0afa52792c7d1485e2c216187765d5
atdi <aurel.avramescu@gmail.com>
2014-12-21 01:21:35 +0100

    Fix command for folder creation

--- manager.py
+++ manager.py
@@ -1,14 +1,38 @@
 # coding: utf-8
-from suittale import init_app, app
+from suittale import init_app, app, db
+from suittale.admin_core import base_path
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
-from suittale.users.models import *
+from flask.ext.script import Command
+from suittale.products.constants import PRODUCTS_IMG_PATH, TEXTURES_IMG_PATH
+import os
+import os.path as op
+
+
+# Command for required folders creation
+class CreateFoldersCommand(Command):
+    def run(self):
+        # Create directory for file fields to use
+        prod_file_path = op.join(base_path, PRODUCTS_IMG_PATH)
+        texture_file_path = op.join(base_path, TEXTURES_IMG_PATH)
+        try:
+            if not os.path.isdir(base_path):
+                os.mkdir(base_path)
+            if not os.path.isdir(prod_file_path):
+                os.mkdir(prod_file_path)
+            if not os.path.isdir(texture_file_path):
+                os.mkdir(texture_file_path)
+        except OSError as err:
+            print("OS error: {0}".format(err))
 
 init_app()
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+manager.add_command('dc', CreateFoldersCommand)
+
+
 
 if __name__ == '__main__':
     manager.run()
