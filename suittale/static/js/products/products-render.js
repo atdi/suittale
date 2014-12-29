595925a0f7725699d7dd62e83d0071c6cb60d861
atdi <aurel.avramescu@gmail.com>
2014-12-29 01:45:34 +0100

    Try to prototype react

---
+++ suittale/static/js/products/products-render.js
@@ -0,0 +1,52 @@
+/**
+ * Created by aurel on 12/29/14.
+ */
+var ProductList = React.createClass({
+  render: function() {
+    var productNodes = this.state.data.map(function (product) {
+      return (
+        <Comment author={product.name}>
+          {product.short_description}
+        </Comment>
+      );
+    });
+    return (
+      <div className="productList">
+        {productNodes}
+      </div>
+    );
+  }
+});
+
+var ProductBox = React.createClass({
+  loadProductsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadProductsFromServer();
+  },
+  render: function() {
+    return (
+      <div className="productBox">
+        <ProductList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+React.render(
+            <ProductBox url="/api/products" />,
+            document.getElementById('example')
+);
