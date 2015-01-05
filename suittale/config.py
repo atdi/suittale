08e34b04ee29bd5153470d97537cd6e7a5e9e837
atdi <aurel.avramescu@gmail.com>
2015-01-05 17:39:59 +0100

    Update openshift database

--- suittale/config.py
+++ suittale/config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
 TESTING = False
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://admin6zauNc9:wvkX75P8nvs3@$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/suittale'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://admin6zauNc9:wvkX75P8nvs3@127.12.252.2:3306/suittale'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SECRET_KEY = '007BOND'
